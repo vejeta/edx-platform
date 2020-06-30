f5564838909c55fb310b2b87a05955ff4c63eecf
Jeff LaJoie <jeffrey.lajoie@gmail.com>
2020-06-30 09:53:57 -0400

    ENT-2893: Updates header branding and href for Enterprise learners

--- openedx/features/enterprise_support/tests/test_utils.py
+++ openedx/features/enterprise_support/tests/test_utils.py
@@ -11,7 +11,9 @@ from django.test.utils import override_settings
 from django.urls import reverse
 
 from openedx.core.djangolib.testing.utils import skip_unless_lms
+from openedx.features.enterprise_support.utils import get_enterprise_learner_portals
 from openedx.features.enterprise_support.tests import FEATURES_WITH_ENTERPRISE_ENABLED
+from openedx.features.enterprise_support.tests.factories import EnterpriseCustomerUserFactory
 from student.tests.factories import UserFactory
 
 
@@ -43,3 +45,41 @@ class TestEnterpriseUtils(TestCase):
         ) as mock_customer_request:
             self.client.get(resource)
             self.assertEqual(mock_customer_request.call_count, expected_calls)
+
+    def test_get_enterprise_learner_portals(self):
+        """
+        Test that only enabled enterprise portals are returned
+        """
+        EnterpriseCustomerUserFactory(user_id=self.user.id)
+        with mock.patch(
+            'openedx.features.enterprise_support.api.get_enterprise_learner_data'
+        ) as mock_get_enterprise_learner_data:
+            mock_get_enterprise_learner_data.return_value = [
+                {
+                    'enterprise_customer': {
+                        'name': 'Enabled Customer',
+                        'enable_learner_portal': True,
+                        'branding_configuration': {
+                            'enterprise_slug': 'enabled_customer',
+                            'logo': 'enabled_logo',
+                        },
+                    },
+                },
+                {
+                    'enterprise_customer': {
+                        'name': 'Disabled Customer',
+                        'enable_learner_portal': False,
+                        'branding_configuration': {
+                            'enterprise_slug': 'disabled_customer',
+                            'logo': 'disabled_logo',
+                        },
+                    },
+                }
+            ]
+            portals = get_enterprise_learner_portals(self.user)
+            self.assertEqual(len(portals), 1)
+            self.assertDictEqual(portals[0], {
+                'name': 'Enabled Customer',
+                'slug': 'enabled_customer',
+                'logo': 'enabled_logo',
+            })
